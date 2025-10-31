5ab3e9d49280e6396eae01d001096c79151cc67b
Jared <okellojared050@gmail.com>
2025-10-30 17:03:29 -0700

    redefine accounts models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
+import uuid
 
 class CustomUser(AbstractUser):
     EMPLOYEE = 'EMP'
@@ -17,6 +19,13 @@ class CustomUser(AbstractUser):
     role = models.CharField(max_length=3, choices=ROLE_CHOICES, default=EMPLOYEE)
     phone = models.CharField(max_length=15, blank=True, null=True)
     department = models.CharField(max_length=100, blank=True, null=True)
+    email_verified = models.BooleanField(default=False)
+    verification_token = models.CharField(max_length=100, blank=True, null=True)
     
     def __str__(self):
         return f"{self.get_full_name()} ({self.role})"
+    
+    def generate_verification_token(self):
+        self.verification_token = str(uuid.uuid4())
+        self.save()
+        return self.verification_token
