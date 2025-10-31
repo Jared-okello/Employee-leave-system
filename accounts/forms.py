05d63dec1df7f8f6c49bb1a7a3bd5409eed169e2
Jared <okellojared050@gmail.com>
2025-10-30 17:05:20 -0700

    added forms.py file

---
+++ accounts/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    phone = forms.CharField(max_length=15, required=False)
+    department = forms.CharField(max_length=100, required=False)
+    
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'first_name', 'last_name', 'phone', 'department', 'role', 'password1', 'password2')
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
